1db756f31efa69b5c2972d66b6c160d15d08ad58
Yeremia Ginting <119391296+yeremiaginting059@users.noreply.github.com>
2023-10-13 09:12:03 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      letterSpacing: {
+        melebar: ".5em",
+      },
+    },
+  },
+  plugins: [],
+};
